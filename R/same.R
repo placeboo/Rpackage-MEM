4a7f79dd42b607a1af133cdfb3ea48cbf3a7d8fe
jiaqi <jiaqiyin1014@gmail.com>
2020-01-09 16:18:43 -0800

    funciton: same

---
+++ R/same.R
@@ -0,0 +1,10 @@
+#' Decide if two values are equivalent
+#'
+#' @param x A numerical vector.
+#' @param y A numerical vector, same dimension as \code{x}.
+#' @param tolerance Difference tolerance, \code{tolerance = .Machine$double.eps^0.5}.
+#' @return \code{TRUE} or \code{FALSE}.
+
+same = function (x, y, tolerance = .Machine$double.eps^0.5) {
+        abs(x - y) < tolerance
+}
