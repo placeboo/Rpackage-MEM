32df299ec28fbb36b3a531448e36aad5c6d6c87c
jiaqi <jiaqiyin1014@gmail.com>
2020-01-16 17:58:57 -0800

    hide it from help page

--- R/same.R
+++ R/same.R
@@ -4,6 +4,7 @@
 #' @param y A numerical vector, same dimension as \code{x}.
 #' @param tolerance Difference tolerance, \code{tolerance = .Machine$double.eps^0.5}.
 #' @return \code{TRUE} or \code{FALSE}.
+#' @noRd
 
 same = function (x, y, tolerance = .Machine$double.eps^0.5) {
         abs(x - y) < tolerance
